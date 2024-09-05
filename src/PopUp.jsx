37955fd5d690b8d7a76636ff79e5b51d886acfa8
FAITH <faithokereke26@gmail.com>
2024-09-05 20:02:30 +0100

    check bugd

--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -7,8 +7,8 @@ export default function PopUp({ setModal,email }) {
   };
   return (
     <div className="grid place-items-center min-h-screen relative">
-      <div className="w-[400px] bg-white rounded-lg md:p-10 p-6  z-10 md:shadow-2xl">
-        <div className="flex flex-col gap-5 mb-32 md:m-0">
+      <div className="w-[400px] bg-white rounded-lg md:p-10   z-10 md:shadow-2xl">
+        <div className="flex flex-col gap-5 mb-32 md:m-0 p-6 md:p-0">
           <img className="w-16" src={sucess} alt="" />
           <h1 className="text-4xl font-bold text-darkBlue pb-4">
             Thanks for subscribing!
@@ -21,9 +21,9 @@ export default function PopUp({ setModal,email }) {
           </p>
         </div>
 
-        <div className="absolute w-full left-0 bottom-10 md:sticky">
+        <div className="absolute w-full bottom-10 md:sticky">
           <button
-            className="bg-darkBlue text-white p-3 rounded-md mt-3 hover:bg-linearGradient w-full"
+            className="bg-darkBlue text-white p-3 rounded-md mt-6 mx-2 md:mx-0 hover:bg-linearGradient w-full"
             onClick={closeModal}
           >
             {" "}
